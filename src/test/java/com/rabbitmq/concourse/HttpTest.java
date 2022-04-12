8a75fc824b10f66f585fb6059149db7627215525
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2022-04-12 09:49:31 +0200

    Allocate more threads for mock HTTP server

--- src/test/java/com/rabbitmq/concourse/HttpTest.java
+++ src/test/java/com/rabbitmq/concourse/HttpTest.java
@@ -36,7 +36,7 @@ public class HttpTest {
         new WireMockServer(
             wireMockConfig()
                 .dynamicPort()
-                .containerThreads(Runtime.getRuntime().availableProcessors()));
+                .containerThreads(Runtime.getRuntime().availableProcessors() * 2));
     wireMockServer.start();
     WireMock.configureFor(wireMockServer.port());
   }
